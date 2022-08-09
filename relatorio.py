071d95836e6fbb095a9451549f1f4d111e614f17
Vésper <nicolasalencar100@gmail.com>
2022-08-09 19:17:39 -0300

    nessa atualização adicionei funcionalidade no botão novo, agora é possível adicionar pessoas.

--- relatorio.py
+++ relatorio.py
@@ -1,16 +1,12 @@
+from asyncio.windows_events import NULL
+from sre_compile import isstring
+import string
 import tkinter
 from tkinter import END, Scrollbar, ttk
 from tkinter.messagebox import NO 
 import sqlite3
 
-def limpar():
-    global colocar_nome, colocar_estudos, colocar_hora, colocar_publicações
-    colocar_nome.delete(0, END)
-    colocar_estudos.delete(0, END)
-    colocar_hora.delete(0, END)
-    colocar_publicações.delete(0, END)
-
-def conectar():
+def montar_tabela():
     global banco_de_dados, cursor
     banco_de_dados = sqlite3.connect("horasdecampo.bd")
     cursor = banco_de_dados.cursor()
@@ -20,10 +16,50 @@ def conectar():
         Publicações INTEGER (20),
         Estudos INTEGER(20)
     );"""
-    )
-    banco_de_dados.close()
+)
+def limpar():
+    global colocar_nome, colocar_estudos, colocar_hora, colocar_publicações, anotações_do_campo
+    colocar_nome.delete(0, END)
+    colocar_estudos.delete(0, END)
+    colocar_hora.delete(0, END)
+    colocar_publicações.delete(0, END)
 
-conectar()
+def conectar():
+    global banco_de_dados, cursor
+    banco_de_dados = sqlite3.connect("horasdecampo.bd")
+    cursor = banco_de_dados.cursor()
+
+def fechar():
+    banco_de_dados.close()
+def add():
+    global colocar_nome, colocar_estudos, colocar_hora, colocar_publicações, anotações_do_campo, nome, estudos, horas, publicações
+    conectar()
+    nome = colocar_nome.get()
+    estudos = colocar_estudos.get()
+    horas = colocar_hora.get()
+    publicações = colocar_publicações.get()
+    if len(nome) != 0:
+        cursor.execute(f""" INSERT INTO relatorio(Nome, Horas, Publicações, Estudos) VALUES (?, ?, ?, ?)
+        """, (nome, estudos, horas, publicações))
+        banco_de_dados.commit()
+        fechar()
+        select()
+        limpar()
+    else:
+        fechar()
+        select()
+        limpar()
+
+def select():
+    global anotações_do_campo
+    anotações_do_campo.delete(*anotações_do_campo.get_children())
+    conectar()
+    list = cursor.execute(""" SELECT Nome, Horas, Publicações, Estudos FROM relatorio; """)
+    for i in list:
+        anotações_do_campo.insert("", END, values=i)
+    fechar()
+
+montar_tabela()
 programa = tkinter.Tk("Relátorio")
 programa.title("Tela")
 programa.configure(background="lightblue")
@@ -44,7 +80,7 @@ botão_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
 botão_buscar = tkinter.Button(frame_colocarinfo, text="Buscar")
 botão_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
 
-botão_novo = tkinter.Button(frame_colocarinfo, text="Novo")
+botão_novo = tkinter.Button(frame_colocarinfo, text="Novo", command=add)
 botão_novo.place(relx=0.5, rely=0.1, relwidth=0.1, relheight=0.15)
 
 botão_alterar = tkinter.Button(frame_colocarinfo, text="Alterar")
@@ -85,7 +121,7 @@ anotações_do_campo.heading("#2", text="Horas")
 anotações_do_campo.heading("#3", text="Publicações")
 anotações_do_campo.heading("#4", text="Estudos")
 
-anotações_do_campo.column("#0", width=1, stretch=NO)
+anotações_do_campo.column("#0", width=1)
 anotações_do_campo.column("#1", width=200)
 anotações_do_campo.column("#2", width=50)
 anotações_do_campo.column("#3", width=125)
